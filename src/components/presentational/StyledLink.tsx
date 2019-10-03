3bb590013471fb86f62aef06253a12a226ecb0f2
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-10-03 11:31:58 -0500

    Styling links

--- src/components/presentational/StyledLink.tsx
+++ src/components/presentational/StyledLink.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { css, cx } from "emotion";
+
+import { useTheme } from "emotion-theming";
+
 export default function StyledLink(props: any) {
+  const theme: any = useTheme();
   return (
     <b>
-      <Link to={props.to}>{props.children}</Link>
+      <Link
+        to={props.to}
+        className={css`
+          padding: 24px;
+          color: ${theme.colors.primary};
+          font-size: 16px;
+          border-radius: 4px;
+          text-decoration: none;
+          &:hover {
+            color: ${theme.colors.tertiary};
+          }
+        `}
+      >
+        {props.children}
+      </Link>
     </b>
   );
 }
