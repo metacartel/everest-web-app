8d0dc7ff5083d5ed940e79e0d6cadcaffa5b37c0
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-10-18 09:43:11 -0500

    Nav bar cleanup

--- src/pages/Proposals.tsx
+++ src/pages/Proposals.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getOpenProposals } from "../helpers/functions";
 
 import InlineStyledLink from "../components/presentational/InlineStyledLink";
+import ENSFetch from "../components/stateful/ENSFetch";
 
 export default function Proposals({ match }: any) {
   return (
@@ -18,6 +19,8 @@ export default function Proposals({ match }: any) {
           </p>
         );
       })}
+      <h1>Data from ENS</h1>
+      <ENSFetch />
     </div>
   );
 }
